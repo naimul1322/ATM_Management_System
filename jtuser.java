e9bed96a01acb1bdc75aeb68deb0e7b0ae1c28ab
Md. Naimul Islam <162487286+naimul1322@users.noreply.github.com>
2026-03-13 15:18:46 +0600

    Add files via upload

---
+++ jtuser.java
@@ -0,0 +1,11 @@
+
+package loginandsignup;
+
+
+class jtuser {
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
